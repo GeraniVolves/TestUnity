ae4a25aa36a68b030b3dbce947e3733d8a0e57ae
GeraniVolves <eremina.kseniya@gmail.com>
2018-09-27 12:26:37 +0300

    add animation for panels

--- Assets/Scripts/TapGroundPanel.cs
+++ Assets/Scripts/TapGroundPanel.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 public class TapGroundPanel : MonoBehaviour {
 
 	public Rigidbody2D barierRigidbody;
 	public float barrierForce = 100f;
+	public SkeletonAnimation [] skeletonAnimation;
+	string currentAnimation = "";
 
 	void Start () {
 		GameObject barrier = GameObject.Find("barrier");
@@ -14,8 +17,19 @@ public class TapGroundPanel : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D stepPanel) {
 			barierRigidbody.AddForce(Vector3.up * barrierForce);
+			SetAnimation("Jump", true);
 	}
+	
 	void OnTriggerExit2D(Collider2D stepPanel) {
 		barierRigidbody.AddForce(Vector3.down * barrierForce);
+		SetAnimation("Idle", true);
+	}
+
+	void SetAnimation(string name, bool loop) {
+		if (name == currentAnimation)
+			return;
+		skeletonAnimation[0].state.SetAnimation(0, name, loop);
+		skeletonAnimation[1].state.SetAnimation(0, name, loop);
+		currentAnimation = name;
 	}
 }
