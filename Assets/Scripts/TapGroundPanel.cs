d9b398bdd82a1cc2e03b53d1dfc088b911617d26
GeraniVolves <eremina.kseniya@gmail.com>
2018-09-27 11:35:54 +0300

    update TapGroundPanel

--- Assets/Scripts/TapGroundPanel.cs
+++ Assets/Scripts/TapGroundPanel.cs
@@ -7,7 +7,6 @@ public class TapGroundPanel : MonoBehaviour {
 	public Rigidbody2D barierRigidbody;
 	public float barrierForce = 100f;
 
-	// Use this for initialization
 	void Start () {
 		GameObject barrier = GameObject.Find("barrier");
 		barierRigidbody = barrier.GetComponent<Rigidbody2D>();
@@ -15,7 +14,6 @@ public class TapGroundPanel : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D stepPanel) {
 		if (stepPanel.gameObject.tag == "Player") {
-			//rb2D.velocity = new Vector2 (0, rb2D.velocity.y);
 			barierRigidbody.AddForce(Vector3.up * barrierForce);
 		}
 	}
